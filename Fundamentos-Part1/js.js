acc5a28bf8a498dba4bce22d436933d1764f80fd
rafajlpz <frilo_94@hotmail.com>
2022-05-09 12:35:25 +0200

    Data type
    Ejercicio data type

--- Fundamentos-Part1/js.js
+++ Fundamentos-Part1/js.js
@@ -1,11 +1,15 @@
-// 1. Declare variables called 'country', 'continent' and 'population' and assign their values according to your own country (population in millions)
+// 1. Declare a variable called 'isIsland' and set its value according to your country. The variable should hold a Boolean value. Also declare a variable 'language', but don't assign it any value yet
 
-// 2. Log their values to the console
+// 2. Log the types of 'isIsland', 'population', 'country' and 'language' to the console.
 
 let country = "Spain";
 let continent = "Europe";
 let population = 47.35+" "+"Millions";
+let isIsland = false;
+let language;
 
 console.log(country,continent,population);
+
+console.log(typeof isIsland, typeof population, typeof country, typeof language);
 
 
