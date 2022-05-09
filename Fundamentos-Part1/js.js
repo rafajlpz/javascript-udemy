60605db025e8a9a7caff33a3f9da8589c8dfbff6
rafajlz <frilo_94@hotmail.com>
2022-05-09 17:45:52 +0200

    Operator precedence
    Pruebas y repaso de operadores y logica

--- Fundamentos-Part1/js.js
+++ Fundamentos-Part1/js.js
@@ -4,14 +4,24 @@
 
 // 3. Try to change one of the changed variables now, and observe what happens
 
-const country = "Spain";
-const continent = "Europe";
-let population = 47.35+" "+"Millions";
-let isIsland = false;
-let language = "Spanish";
+// const country = "Spain";
+// const continent = "Europe";
+// let population = 47.35+" "+"Millions";
+// let isIsland = false;
+// let language = "Spanish";
 
-console.log(country,continent,population);
+// console.log(country,continent,population);
 
-console.log(typeof isIsland, typeof population, typeof country, typeof language);
+// console.log(typeof isIsland, typeof population, typeof country, typeof language);
+
+let x = 10 + 5 // 15
+x += 10 //Es lo mismo que x = x + 10 = 25
+x *= 4; //Es lo mismo que  x = x * 4 = 100
+x++; //Es a;adirle uno mas. Osea 101
+
+console.log(x);
+
+let a, y;
+a = y = 25 - 10 - 5; // x = y = 10, x = 10 //El codigo se lee de derecha a izquierda por eso a e y dan 10 en este caso, sino fuese al reves y seria unndefined
 
 
