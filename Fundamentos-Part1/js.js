55587b0acd814be887a2615cf41926645e251db9
rafajlpz <frilo_94@hotmail.com>
2022-05-09 13:15:48 +0200

    let var y const, ejercicio

--- Fundamentos-Part1/js.js
+++ Fundamentos-Part1/js.js
@@ -1,12 +1,14 @@
-// 1. Declare a variable called 'isIsland' and set its value according to your country. The variable should hold a Boolean value. Also declare a variable 'language', but don't assign it any value yet
+// 1. Set the value of 'language' to the language spoken where you live (some countries have multiple languages, but just choose one)
 
-// 2. Log the types of 'isIsland', 'population', 'country' and 'language' to the console.
+// 2. Think about which variables should be const variables (which values will never change, and which might change?). Then, change these variables to const.
 
-let country = "Spain";
-let continent = "Europe";
+// 3. Try to change one of the changed variables now, and observe what happens
+
+const country = "Spain";
+const continent = "Europe";
 let population = 47.35+" "+"Millions";
 let isIsland = false;
-let language;
+let language = "Spanish";
 
 console.log(country,continent,population);
 
