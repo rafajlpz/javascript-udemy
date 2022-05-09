37326ea7545882f636f5dd66cc2234d5878daf59
rafajlpz <frilo_94@hotmail.com>
2022-05-09 12:17:59 +0200

    Ejercicio 1 Values and Variables

--- Fundamentos-Part1/js.js
+++ Fundamentos-Part1/js.js
@@ -1,2 +1,11 @@
-let js = 'amazing';
-if (js === 'amazing') alert ('JavaScript es divertido!');
+// 1. Declare variables called 'country', 'continent' and 'population' and assign their values according to your own country (population in millions)
+
+// 2. Log their values to the console
+
+let country = "Spain";
+let continent = "Europe";
+let population = 47.35+" "+"Millions";
+
+console.log(country,continent,population);
+
+
