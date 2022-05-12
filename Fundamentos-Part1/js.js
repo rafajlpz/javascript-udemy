74a99158a399f0f5656385cc332f97aef86ed00b
rafajlpz <frilo_94@hotmail.com>
2022-05-12 12:24:38 +0200

    Practica if/else

--- Fundamentos-Part1/js.js
+++ Fundamentos-Part1/js.js
@@ -1,62 +1,23 @@
-/*
-Coding Challenge #1
-
-Mark and John are trying to compare their BMI (Body Mass Index), which is
-calculated using the formula:
-
-BMI = mass / height ** 2 = mass / (height * height) (mass in kg
-and height in meter).
-Your tasks:
-
-1. Store Mark's and John's mass and height in variables
-
-2. Calculate both their BMIs using the formula (you can even implement both
-versions)
-
-3. Create a Boolean variable 'markHigherBMI' containing information about
-whether Mark has a higher BMI than John.
-
-Test data:
-§ Data 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95
-m tall.
-§ Data 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76
-m tall.
-*/
-
-// let massMark = 78;
-// let massJohn = 95;
-
-// let heightMark = 1.69;
-// let heightJohn = 1.88;
-
-// // BMI = mass / height ** 2 = mass / (height * height);
-
-// let markBMI = massMark / (heightMark * heightMark);
-// console.log(markBMI);
-
-// let johnkBMI = massJohn / (heightJohn * heightJohn);
-// console.log(johnkBMI);
 
 
-// let markHigherBMI = markBMI > johnkBMI;
+const edad = 15;
+const edadPermitida = edad >= 18;
 
-// console.log(markHigherBMI);
+if(edadPermitida){
+    console.log(`La persona indicada puede conducir `)
+} else {
+    const faltan = 18 - edad;
+    console.log(`La persona no cumple el minimo de edad, te falta ${faltan} años`)
+}
 
-const firstName = 'Rafael';
-const job = 'developer';
-const birthYear = 1994;
-const year = 2022;
+const nacimiento = 1994;
+let siglo;
 
-const rafa = `Soy ${firstName}, 
-trabajo en Cersia como ${job} 
-y mi edad es ${year-birthYear}`;
-
-console.log(rafa);
-// Para hacer un salto de linea o nueva linea en javascript hay que usar \n\ al final de la expresion
-console.log('Probando salto de linea \n\
-probando...\n\
-probando...');
-// Pero usando template literal se consigue lo mismo
+if (nacimiento>=2000){
+    console.log(`Eres del ${siglo = 21}`)
+} else {
+    console.log(`Eres del siglo ${siglo = 20}`);
+}
 
 
 
