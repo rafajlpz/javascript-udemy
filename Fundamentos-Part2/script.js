9f16cfed9463bb15350049207930ed4e8c5dff34
rafajlz <frilo_94@hotmail.com>
2022-05-20 23:03:06 +0200

    Funciones y declaraciones de funciones

--- Fundamentos-Part2/script.js
+++ Fundamentos-Part2/script.js
@@ -1,7 +1,20 @@
 "use strict";
 
-let tieneCarnet = false;
-const testAprobado = true;
+// let tieneCarnet = false;
+// const testAprobado = true;
 
-if(testAprobado) tieneCarnet = true;
-if(tieneCarnet) console.log('Puedo conducir')
+// if(testAprobado) tieneCarnet = true;
+// if(tieneCarnet) console.log('Puedo conducir')
+
+function calcularEdad1(anyo){
+    return 2022 - anyo;
+}
+const edad1 = calcularEdad1(1994);
+console.log(edad1);
+
+const calcularedad2 = function (anyo){
+    return 2022 - anyo;
+
+}
+const edad2 = calcularedad2(1999);
+console.log(edad1,edad2);
