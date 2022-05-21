cdb355615c7289de45421c2da4269437361fb332
rafajlz <frilo_94@hotmail.com>
2022-05-21 18:19:05 +0200

    Funcion flecha pruebas simples

--- Fundamentos-Part2/script.js
+++ Fundamentos-Part2/script.js
@@ -6,15 +6,26 @@
 // if(testAprobado) tieneCarnet = true;
 // if(tieneCarnet) console.log('Puedo conducir')
 
-function calcularEdad1(anyo){
-    return 2022 - anyo;
-}
-const edad1 = calcularEdad1(1994);
-console.log(edad1);
+// function calcularEdad1(anyo){
+//     return 2022 - anyo;
+// }
+// const edad1 = calcularEdad1(1994);
+// console.log(edad1);
 
-const calcularedad2 = function (anyo){
-    return 2022 - anyo;
+// const calcularedad2 = function (anyo){
+//     return 2022 - anyo;
 
+// }
+// const edad2 = calcularedad2(1999);
+// console.log(edad1,edad2);
+
+// Funcion Flecha
+// const calcularEdad = anyo => 2022 - anyo;
+
+const nuevaFuncionFlecha = anyo =>{
+    const edad = 2022 - anyo;
+    const jubilacion = 100 - edad;
+    return `Tu edad es de ${edad},
+    tu edad de jubilacion seria a los ${jubilacion}`;
 }
-const edad2 = calcularedad2(1999);
-console.log(edad1,edad2);
+console.log(nuevaFuncionFlecha(1994));
