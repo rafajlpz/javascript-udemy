d9f7b85355c0eb3a19662363cc3f2e3f3423a05b
rafajlz <frilo_94@hotmail.com>
2022-06-04 18:18:37 +0200

    Coding Challenge #4

--- Fundamentos-Part2/script.js
+++ Fundamentos-Part2/script.js
@@ -320,18 +320,79 @@ GOOD LUCK �
 //   }
 // }
 
-let rep = 1;
-while (rep <= 10) {
-  // console.log(`WHILE Ejercicio con peso repeticion ${rep}`);
-  rep++;
+// let rep = 1;
+// while (rep <= 10) {
+//   // console.log(`WHILE Ejercicio con peso repeticion ${rep}`);
+//   rep++;
+// }
+
+// let dado = Math.trunc(Math.random() * 6) +1;
+
+// while (dado !== 6){
+//   console.log(`You rolled a ${dado}`);
+//   dado = Math.trunc(Math.random() * 6) +1;
+//   if(dado === 6) console.log('Bucle esta cerca de acabar...')
+
+// }
+
+/* Coding Challenge #4
+
+Let's improve Steven's tip calculator even more, this time using loops!
+
+Your tasks:
+1. Create an array 'bills' containing all 10 test bill values
+
+2. Create empty arrays for the tips and the totals ('tips' and 'totals')
+
+3. Use the 'calcTip' function we wrote before (no need to repeat) to calculate 
+tips and total values (bill + tip) for every bill value in the bills array. Use a for loop to perform the 10 calculations!
+
+Test data: 22, 295, 176, 440, 37, 105, 10, 1100, 86 and 52
+
+Hints: Call ‘calcTip ‘in the loop and use the push method to add values to the 
+tips and totals arrays �
+Bonus:
+
+4. Bonus: Write a function 'calcAverage' which takes an array called 'arr' as 
+an argument. This function calculates the average of all numbers in the given 
+array. This is a difficult challenge (we haven't done this before)! Here is how to 
+solve it:
+
+4.1. First, you will need to add up all values in the array. To do the addition, 
+start by creating a variable 'sum' that starts at 0. Then loop over the 
+array using a for loop. In each iteration, add the current value to the 
+'sum' variable. This way, by the end of the loop, you have all values 
+added together
+
+4.2. To calculate the average, divide the sum you calculated before by the 
+length of the array (because that's the number of elements)
+
+4.3. Call the function with the 'totals' array
+GOOD LUCK 
+*/
+
+const calcPropina = (propina) => {
+  return propina >= 50 && propina <= 300 ? propina * 0.15 : propina * 0.2;
+};
+
+const facturas = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const propinas = [];
+const totales = [];
+
+for (let i = 0; facturas.length > i; i++) {
+  const propina = calcPropina(facturas[i]);
+  propinas.push(propina);
+  totales.push(propina + facturas[i]);
 }
 
-let dado = Math.trunc(Math.random() * 6) +1;
+console.log(facturas, propinas, totales);
 
-
-while (dado !== 6){
-  console.log(`You rolled a ${dado}`);
-  dado = Math.trunc(Math.random() * 6) +1;
-  if(dado === 6) console.log('Bucle esta cerca de acabar...')
-
+const calcAverage = function(arr){
+  let sum = 0;
+  for(let i = 0; i < arr.length; i++){
+    sum = sum + arr[i];
+  }
+  return sum / arr.length
 }
+calcAverage([2,6,2]);
+console.log(calcAverage(totales));
