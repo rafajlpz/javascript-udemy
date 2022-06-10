87e8e3673a593f7b5b8cc99e7004f01f71d0ef52
rafajlz <frilo_94@hotmail.com>
2022-06-11 01:15:08 +0200

    Seguimiento juego adivina mi numero

--- Dom-EventsFundaments/Guess-My-Number/starter/script.js
+++ Dom-EventsFundaments/Guess-My-Number/starter/script.js
@@ -1,23 +1,30 @@
 'use strict';
 
-const parrafo = document.querySelector('.message').textContent;
+const btnCheck = document.querySelector('.check');
+const btnAgain = document.querySelector('.again');
+const contenido = document.querySelector('.contenido');
 
-console.log(parrafo);
+const numeroSecreto = Math.floor(Math.random() * 80) + 1;
+console.log(numeroSecreto);
 
-const btnCheck = document.querySelector(".check");
-const btnAgain = document.querySelector(".again")
+const vidas = Number(document.querySelector('.score').textContent);
 
-btnCheck.onclick = () =>{
-    let adivina = [];
-    for(let i = 0; adivina>1 && adivina<=20; i++){
-        adivina.push(i);
-    }
+
+btnCheck.onclick = () => {
+  const guess = Number(document.querySelector('.guess').value);
+  if (!guess) {
+    document.querySelector('.message').textContent = `🚫 Introduce un numero!!`;
+  } else if (guess === numeroSecreto) {
+    document.querySelector('.number').textContent = guess;
+    document.querySelector('.adivina').textContent = `✨ Acertaste!!!`;
+    contenido.style.backgroundColor = `green`;
+  } else if (guess !== numeroSecreto) {
+    document.querySelector('.adivina').textContent = `Intentalo otra vez...`;
+    contenido.style.backgroundColor = `red`;
+    // btnCheck.className.disabled;
+  }
 };
 
-btnAgain.onclick = () =>{
-    console.log("Funciona sgfdsfsa")
+btnAgain.onclick = () => {
+  location.reload();
 };
-
-const dameNumero = () =>{
-    
-}
