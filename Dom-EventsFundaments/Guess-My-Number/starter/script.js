8da00bce96ad6eca8cd6faead1ab6207300e8e36
rafajlpz <frilo_94@hotmail.com>
2022-06-10 14:42:42 +0200

    Inicio Guess My Number DOM

--- Dom-EventsFundaments/Guess-My-Number/starter/script.js
+++ Dom-EventsFundaments/Guess-My-Number/starter/script.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const parrafo = document.querySelector('.message').textContent;
+
+console.log(parrafo);
+
+const btnCheck = document.querySelector(".check");
+const btnAgain = document.querySelector(".again")
+
+btnCheck.onclick = () =>{
+    let adivina = [];
+    for(let i = 0; adivina>1 && adivina<=20; i++){
+        adivina.push(i);
+    }
+};
+
+btnAgain.onclick = () =>{
+    console.log("Funciona sgfdsfsa")
+};
+
+const dameNumero = () =>{
+    
+}
