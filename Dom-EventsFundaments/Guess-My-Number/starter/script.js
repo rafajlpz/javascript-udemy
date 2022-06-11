ef0d60a98e5cd71351d6015dcac5840633597105
rafajlz <frilo_94@hotmail.com>
2022-06-11 20:16:34 +0200

    Actualizacion juego adivina mi numero

--- Dom-EventsFundaments/Guess-My-Number/starter/script.js
+++ Dom-EventsFundaments/Guess-My-Number/starter/script.js
@@ -7,21 +7,36 @@ const contenido = document.querySelector('.contenido');
 const numeroSecreto = Math.floor(Math.random() * 80) + 1;
 console.log(numeroSecreto);
 
-const vidas = Number(document.querySelector('.score').textContent);
-
+let vidas = 5;
 
 btnCheck.onclick = () => {
   const guess = Number(document.querySelector('.guess').value);
+
   if (!guess) {
     document.querySelector('.message').textContent = `🚫 Introduce un numero!!`;
   } else if (guess === numeroSecreto) {
-    document.querySelector('.number').textContent = guess;
     document.querySelector('.adivina').textContent = `✨ Acertaste!!!`;
     contenido.style.backgroundColor = `green`;
-  } else if (guess !== numeroSecreto) {
-    document.querySelector('.adivina').textContent = `Intentalo otra vez...`;
-    contenido.style.backgroundColor = `red`;
-    // btnCheck.className.disabled;
+  } else if (guess > numeroSecreto) {
+    if (vidas > 1) {
+      document.querySelector('.adivina').textContent = `Te pasaste...`;
+      vidas--;
+      document.querySelector('.score').textContent = vidas;
+    } else {
+      document.querySelector('.adivina').textContent = `💥 Perdiste...!!!`;
+      document.querySelector('.score').textContent = 0;
+      contenido.style.backgroundColor = `red`;
+    }
+  } else if (guess < numeroSecreto) {
+    if (vidas > 1) {
+      document.querySelector('.adivina').textContent = `Es mas alto...`;
+      vidas--;
+      document.querySelector('.score').textContent = vidas;
+    } else {
+      document.querySelector('.adivina').textContent = `💥 Perdiste...!!!`;
+      document.querySelector('.score').textContent = 0;
+      contenido.style.backgroundColor = `red`;
+    }
   }
 };
 
