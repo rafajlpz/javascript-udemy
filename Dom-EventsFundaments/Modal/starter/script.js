c691a0bf72483c91248b924df653bbbdcadbf193
rafajlz <frilo_94@hotmail.com>
2022-08-18 05:23:10 +0200

    Realizado Modal, video #80

--- Dom-EventsFundaments/Modal/starter/script.js
+++ Dom-EventsFundaments/Modal/starter/script.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const cubrir = document.querySelector('.overlay');
+const modal = document.querySelector('.modal');
+const abrirModal = document.querySelectorAll('.show-modal');
+const cerrarModal = document.querySelector('.close-modal');
+
+const cerrar = () => {
+  modal.classList.add('hidden');
+  cubrir.classList.add('hidden');
+};
+
+const abrir = () => {
+  modal.classList.remove('hidden');
+  cubrir.classList.remove('hidden');
+};
+
+for (let i = 0; i < abrirModal.length; i++) {
+  abrirModal[i].onclick = () => abrir();
+}
+
+cerrarModal.onclick = () => cerrar();
+cubrir.onclick = () => cerrar();
