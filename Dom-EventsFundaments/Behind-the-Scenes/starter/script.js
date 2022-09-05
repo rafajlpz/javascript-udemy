185f3cedb86308d644a0cbb9568d156668184827
rafajlz <frilo_94@hotmail.com>
2022-09-05 19:16:49 +0200

    Pruebas detras de javascript

--- Dom-EventsFundaments/Behind-the-Scenes/starter/script.js
+++ Dom-EventsFundaments/Behind-the-Scenes/starter/script.js
@@ -1,1 +1,24 @@
 'use strict';
+
+const firstName = 'Rafael';
+calcAge(1994);
+
+function calcAge(birthYear) {
+  const age = 2022 - birthYear;
+  function printAge() {
+    const output = `${firstName}, tienes ${age}, nacido en ${birthYear}`;
+    console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+        const string = `Eres un millenial, ${firstName}`
+        console.log(string)
+
+        function add (a, b){
+            return a + b;
+        }
+        console.log(add(2 , 3))
+    }
+  }
+  printAge();
+  return age;
+}
