f27821d7b7a0ea809940020c35870317a1cab064
rafajlz <frilo_94@hotmail.com>
2022-11-13 20:23:48 +0100

    Coding Challenge #1 Data structures and modern operators

--- Dom-EventsFundaments/Data-Structures-Operators/starter/script.js
+++ Dom-EventsFundaments/Data-Structures-Operators/starter/script.js
@@ -1,106 +1,86 @@
 'use strict';
 
-// Data needed for a later exercise
-const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
-
-// Data needed for first part of the section
-const restaurant = {
-  name: 'Classico Italiano',
-  location: 'Via Angelo Tavanti 23, Firenze, Italy',
-  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  },
-  order: function (starterIndex, mainIndex) {
-    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
-  },
-  orderDelivery: function (obj) {
-    // console.log(obj)
-  },
-  orderPasta: function (ing1, ing2, ing3) {
-    console.log(`Pasta deliciosa con ${ing1},${ing2},${ing3}`);
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnabry',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnabry', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
   },
 };
 
-// restaurant.orderDelivery({
-//   time:'22:30',
-//   addres: 'Via del Sol, 21',
-//   mainIndex: 2,
-//   starterIndex: 2
-// })
+// 1. Create one player array for each team (variables 'players1' and 'players2')
+const [t1Players, t2Players] = game.players;
+console.log(t1Players, t2Players);
 
-// const arr = [2, 3, 4];
-// const a = arr[0];
-// const b = arr[1];
-// const c = arr[2];
 
-// const [x, y, z] = arr;
-// // console.log(x, y, z);
+// 2. The first player in any player array is the goalkeeper and the others are field  players. For Bayern Munich (team 1) create one variable ('gk') with the  goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10 field players
+const [gk, ...fieldPlayers] = t1Players;
+console.log(gk, fieldPlayers);
 
-// let [main, , secondary] = restaurant.categories;
-// // console.log(main, secondary);
 
-// [main, secondary] = [secondary, main];
-// // console.log(main, secondary);
+// 3. Create an array 'allPlayers' containing all players of both teams (22 players)
+const allPlayers = [...t1Players, ...t2Players];
+console.log(allPlayers);
 
-// const [starter, mainCourse] = restaurant.order(2, 0);
-// // console.log(starter, mainCourse);
 
-// const nested = [2, 4, [5, 6]];
-// // const [i, , j] = nested;
-// // console.log(i, j);
-// const [i, , [j, k]] = nested;
-// // console.log(i, j, k);
+// 4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic
+const substitute = ['Thiago', 'Coutinho', 'Perisic'];
+const players1Final = [...t1Players, ...substitute];
+console.log(players1Final);
 
-// const {name, openingHours, categories} = restaurant
-// // console.log(name, openingHours, categories)
 
-// const {name: restaurantName, openingHours: hours, ctegories: tags} = restaurant;
+// 5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
+const {
+  odds: { team1, team2, x: draw },
+} = game;
+console.log(team1, team2, draw);
 
-// const {menu = [], starterMenu: starters = []} = restaurant;
-// // console.log(menu, starters)
+// -> Tambien estaria correcto esta forma <-
+// const team1 = game.odds.team1;
+// const team2 = game.odds.team2;
+// const draw = game.odds.x;
+// console.log(team1, team2, draw)
 
-// Spread Operator
-const arr = [7, 8, 9];
 
-const newArray = [1, 2, ...arr];
-// console.log(newArray);
+// 6. Write a function ('printGoals') that receives an arbitrary number of player names (not an array) and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in)
+const printGoals = function (...players) {
+  console.log(`${players.length} goles en el partido`);
+};
+printGoals(...game.scored);
 
-// console.log(...newArray)
 
-const newMainMenu = ['Jamon', 'Pescado', ...restaurant.mainMenu];
-// console.log(newMainMenu);
-
-// Copiar array
-const mainMenuCopy = [...restaurant.mainMenu];
-// Unir arrays
-const unionMenus = [...restaurant.mainMenu, ...newMainMenu];
-// console.log(unionMenus)
-
-const ingredients = [
-  prompt(`Hagamos pasta con ingrediente 1?`),
-  prompt(`ingrediente 2?`),
-  prompt(`ingrediente 3?`),
-];
-
-console.log(ingredients);
-// restaurant.orderPasta(ingredients[0], ingredients[1],ingredients[2])
-restaurant.orderPasta(...ingredients)
-
-// Objetos
-const newRestaurant = {anyo: 2026, ...restaurant, founder:'Rafael Lopez',}
-console.log(newRestaurant)
+//7. The team with the lower odd is more likely to win. Print to the console which team is more likely to win, without using an if/else statement or the ternary operator.
+team1 < team2 && console.log(`El equipo 1 tiene mas probabilidades de ganar`)
