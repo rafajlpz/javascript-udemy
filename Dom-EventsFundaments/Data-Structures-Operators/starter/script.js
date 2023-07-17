63f37863e36c3c8c3640e33554e58b0013aeaf7f
Rafa <rafa@Galaxy-Note9.localdomain>
2023-07-17 17:30:46 +0200

    Prueba new Objects

--- Dom-EventsFundaments/Data-Structures-Operators/starter/script.js
+++ Dom-EventsFundaments/Data-Structures-Operators/starter/script.js
@@ -1,4 +1,4 @@
-'use strict';
+// 'use strict';
 
 const game = {
   team1: 'Bayern Munich',
@@ -43,30 +43,26 @@ const game = {
 
 // 1. Create one player array for each team (variables 'players1' and 'players2')
 const [t1Players, t2Players] = game.players;
-console.log(t1Players, t2Players);
-
+// console.log(t1Players, t2Players);
 
 // 2. The first player in any player array is the goalkeeper and the others are field  players. For Bayern Munich (team 1) create one variable ('gk') with the  goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10 field players
 const [gk, ...fieldPlayers] = t1Players;
-console.log(gk, fieldPlayers);
-
+// console.log(gk, fieldPlayers);
 
 // 3. Create an array 'allPlayers' containing all players of both teams (22 players)
 const allPlayers = [...t1Players, ...t2Players];
-console.log(allPlayers);
-
+// console.log(allPlayers);
 
 // 4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic
 const substitute = ['Thiago', 'Coutinho', 'Perisic'];
 const players1Final = [...t1Players, ...substitute];
-console.log(players1Final);
-
+// console.log(players1Final);
 
 // 5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
 const {
   odds: { team1, team2, x: draw },
 } = game;
-console.log(team1, team2, draw);
+// console.log(team1, team2, draw);
 
 // -> Tambien estaria correcto esta forma <-
 // const team1 = game.odds.team1;
@@ -74,13 +70,59 @@ console.log(team1, team2, draw);
 // const draw = game.odds.x;
 // console.log(team1, team2, draw)
 
-
 // 6. Write a function ('printGoals') that receives an arbitrary number of player names (not an array) and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in)
 const printGoals = function (...players) {
-  console.log(`${players.length} goles en el partido`);
+  // console.log(`${players.length} goles en el partido`);
 };
 printGoals(...game.scored);
 
-
 //7. The team with the lower odd is more likely to win. Print to the console which team is more likely to win, without using an if/else statement or the ternary operator.
-team1 < team2 && console.log(`El equipo 1 tiene mas probabilidades de ganar`)
+// team1 < team2 && console.log(`El equipo 1 tiene mas probabilidades de ganar`)
+
+// Retoamando restaurante
+// Data needed for a later exercise
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+const weekdays = ['mon', 'tue', 'wed', 'thu', 'sat', 'sun'];
+
+const openingHours = {
+  [weekdays[3]]: {
+    open: 12,
+    close: 22,
+  },
+  fri: {
+    open: 11,
+    close: 23,
+  },
+  sat: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
+
+// Data needed for first part of the section
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  // ES6 enhaced objet literals
+  openingHours: openingHours,
+
+  order(starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+  orderDelivery(obj) {
+    // console.log(obj)
+  },
+  orderPasta(ing1, ing2, ing3) {
+    console.log(`Pasta deliciosa con ${ing1},${ing2},${ing3}`);
+  },
+};
+
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+
+for (const item of menu) console.log(item);
--- Dom-EventsFundaments/OOP/starter/script.js
+++ Dom-EventsFundaments/OOP/starter/script.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const Persona = function (primerNombre, anioNacimiento) {
+  this.primerNombre = primerNombre;
+  this.anioNacimiento = anioNacimiento;
+
+  
+};
+
+const rafael = new Persona('Rafael', 1994);
+console.log(rafael);
+
+const ana = new Persona('Ana', 1996);
+const sergio = new Persona('Sergio', 2000);
+console.log(ana, sergio);
+
+const jay = ('Jay')
+
+console.log(rafael instanceof Persona)
+console.log(jay instanceof Persona)
