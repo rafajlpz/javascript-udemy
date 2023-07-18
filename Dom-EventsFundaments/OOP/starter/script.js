2bedbc63b393d0e6d6502adcb0132ed599095d8a
rafajlpz <frilo_94@hotmail.com>
2023-07-18 12:33:10 +0200

    Coding Challenge #1 POO

--- Dom-EventsFundaments/OOP/starter/script.js
+++ Dom-EventsFundaments/OOP/starter/script.js
@@ -14,3 +14,35 @@ const jay = 'Jay';
 console.log(ana, sergio);
 console.log(rafael instanceof Persona);
 console.log(jay instanceof Persona);
+
+// Prototypes
+Persona.prototype.calcularEdad = function () {
+  console.log(2023 - this.anioNacimiento);
+};
+
+const arr = [3, 6, 4, 5, 6, 9, 3];
+console.log(arr.__proto__);
+console.log(arr.__proto__ === Array.prototype);
+
+// 1. Use a constructor function to implemente a Car. A car has a make and a speed property. The speed property is the current speed of the car in km/h;
+// Coding Challenge #1 POO
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+Car.prototype.accelerate = function(){
+    this.speed += 10;
+    console.log(`${this.make} is going at ${this.speed} km/h`)
+}
+
+Car.prototype.brake = function(){
+    this.speed -= 5;
+    console.log(`${this.make} is going at ${this.speed} km/h`)
+}
+
+const bmw = new Car('BMW', 120);
+const mercedes = new Car('Mercedes', 95);
+
+bmw.accelerate();
+bmw.accelerate();
