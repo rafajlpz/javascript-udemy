46714498b5e0f771ee8cd19a3b29c78413f87121
rafajlpz <frilo_94@hotmail.com>
2023-07-19 12:14:10 +0200

    Instalacion npm leaflet, introduccion mapa

--- Dom-EventsFundaments/Mapty/starter/script.js
+++ Dom-EventsFundaments/Mapty/starter/script.js
@@ -18,6 +18,18 @@ if (navigator.geolocation) {
       const { longitude } = position.coords;
       // Cambiar este console.log por link de google con su lat y long
       console.log(latitute, longitude);
+
+      const map = L.map('map').setView([51.505, -0.09], 13);
+
+      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker([51.5, -0.09])
+        .addTo(map)
+        .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+        .openPopup();
     },
     function () {
       alert('No se puede acceder a su localizacion!');
